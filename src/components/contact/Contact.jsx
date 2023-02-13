d232f6d8de6edceeb3fefe7ad4e0c35a57532351
John Seth Salazar <salazarjohnseth@gmail.com>
2023-02-13 08:43:30 +0800

    Polishing 3

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -44,7 +44,7 @@ const Contact = () => {
           <article className="contact__option">
             <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
-            <h5>+63915478698</h5>
+            <h5>+639159417413</h5>
             <a href='https://api.whatsapp.com/send?phone+639159417413' target='_blank'>Send a message</a>
           </article>
         </div>

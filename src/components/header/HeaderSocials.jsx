bb4ad59bc153827e92fbfc1f3fc98c6b939d8e31
John Seth Salazar <salazarjohnseth@gmail.com>
2023-05-15 18:56:35 +0800

    Added Gumroad profile

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -3,6 +3,7 @@ import {BsLinkedin} from 'react-icons/bs'
 import {BsTwitter} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
 import {SiHashnode} from 'react-icons/si'
+import {TbBrandGumroad} from 'react-icons/tb'
 
 const HeaderSocials = () => {
   return (
@@ -11,6 +12,7 @@ const HeaderSocials = () => {
         <a href='https://github.com/johnsethsalazar' target="_blank"><FaGithub /></a>
         <a href='https://twitter.com/salazarjohnseth' target="_blank"><BsTwitter /></a>
         <a href='https://johnsethsalazar.hashnode.dev/' target="_blank"><SiHashnode /></a>
+        <a href='https://johnsethsalazar.gumroad.com/' target="_blank"><TbBrandGumroad /></a>
     </div>
   )
 }
